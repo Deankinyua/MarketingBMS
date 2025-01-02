57b5731c9b17a89638d4bc942f4e49b869fcc3d6
Dean Kinyua <kinyuadean@gmail.com>
2025-01-02 22:33:49 +0300

    changed origin to point to aws ip

--- config/runtime.exs
+++ config/runtime.exs
@@ -118,7 +118,7 @@ if config_env() == :prod do
 
   config :marketingbsm, MarketingbsmWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
-    check_origin: ["http://13.49.63.43:9484"],
+    check_origin: ["http://13.48.254.182:9484"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
