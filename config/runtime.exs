7b8f789f8ca0f396bb68908532aeeed4232dbd50
Dean Kinyua <kinyuadean@gmail.com>
2025-01-03 06:30:41 +0300

    changed to custom 443

--- config/runtime.exs
+++ config/runtime.exs
@@ -113,11 +113,11 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "localhost"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :marketingbsm, MarketingbsmWeb.Endpoint,
-    url: [host: host, port: 80, scheme: "https"],
+    url: [host: host, port: 443, scheme: "https"],
     check_origin: ["http://13.48.254.182:9484"],
     http: [
       # Enable IPv6 and bind on all interfaces.
